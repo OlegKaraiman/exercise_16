c0b4758913294bd8a157eceed538e8a6ada714db
OlegKaraiman <K232629@users.noreply.github.com>
2020-03-25 14:49:32 +0200

    DOM

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
-let msg = document.querySelector("div.tooltip");
-let span = document.createElement("span");
-msg.addEventListener("mouseover", getMsg);
+const msg = document.querySelector('div.tooltip');
+const span = document.createElement('span');
+msg.addEventListener('mouseover', getMsg);
 function getMsg() {
-  span.setAttribute("class", "tooltip-content");
-  span.innerHTML = msg.getAttribute('data-tooltip');
-  msg.append(span);
+    span.setAttribute('class', 'tooltip-content');
+    span.innerHTML = msg.getAttribute('data-tooltip');
+    msg.append(span);
 }
-msg.addEventListener("mouseout", outMsg);
+msg.addEventListener('mouseout', outMsg);
 function outMsg() {
-  span.parentNode.removeChild(span);
+    span.parentNode.removeChild(span);
 }
